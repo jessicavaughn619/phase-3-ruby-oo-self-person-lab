03a3bf8860e3a3d8afde73e5d263d1092f28fade
Jessica Vaughn <jessica.vaughn619@gmail.com>
2023-09-14 13:47:23 -0600

    Solution

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,90 @@
-# your code goes here
+class Person
+    attr_reader :name
+    attr_accessor :bank_account, :happiness, :hygiene
+
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def name
+        @name
+    end
+
+    def happiness=(value)
+        if (0...11).cover?(value)
+            @happiness = value
+        elsif value > 10
+            @happiness = 10
+        elsif value < 0
+            @happiness = 0
+        end
+    end
+
+    def hygiene=(value)
+        if (0...11).cover?(value)
+            @hygiene = value
+        elsif value > 10
+            @hygiene = 10
+        elsif value < 0
+            @hygiene = 0
+        end
+    end
+
+    def happy?
+        if @happiness > 7
+            true
+        else
+            false
+        end
+    end
+
+    def clean?
+        if @hygiene > 7
+            true
+        else
+            false
+        end
+    end
+
+    def get_paid(amount)
+        self.bank_account = self.bank_account + amount
+        "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene = self.hygiene + 4
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.hygiene = self.hygiene - 3
+        self.happiness = self.happiness + 2
+        "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(friend)
+        self.happiness = self.happiness + 3
+        friend.happiness = friend.happiness + 3
+        "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(person, topic)
+        case topic
+        when "politics"
+            self.happiness = self.happiness - 2
+            person.happiness = person.happiness - 2
+            "blah blah partisan blah lobbyist"
+        when "weather"
+            self.happiness = self.happiness + 1
+            person.happiness = person.happiness + 1
+            "blah blah sun blah rain"
+        else
+            "blah blah blah blah blah"
+        end
+    end
+
+end
+
